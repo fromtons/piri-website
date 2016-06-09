c3554e89bd7299dfb86ecbab754f4f4124003f16
Noémie Diaz <noemiediaz4@gmail.com>
2016-06-09 11:20:54 +0200

    Add Slider and Parallax

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,55 @@
-/**
- * Created by jerek0 on 07/05/2015.
- */
+import 'gsap';
+import ScrollMagic from 'scrollmagic';
+import Flickity from 'flickity';
+require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
+require('scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators');
 
-import Test from "./classes/test";
+let app = {
+  init: function() {
+    this.bindUIActions();
+  },
 
-let myTest = new Test(1, "Noémie");
+  bindUIActions: function() {
+    this.registerParallax();
+    this.registerSlider();
+  },
 
-console.log(myTest);
-myTest.sayHello();
+  registerParallax: function() {
+    var controller = new ScrollMagic.Controller();
+    // build tween
+    var tween = new TimelineMax ()
+    .add([
+      TweenMax.fromTo("#layer_1", 1, {top: 1500}, {top: -200, ease: Linear.easeNone}),
+      TweenMax.fromTo("#layer_2", 1, {top: 500}, {top: -200, ease: Linear.easeNone}),
+      TweenMax.fromTo("#layer_3", 1, {top: 50}, {top: -200, ease: Linear.easeNone}),
+      TweenMax.fromTo("#layer_4", 1, {top: 350}, {top: 100, ease: Linear.easeNone}),
+      TweenMax.fromTo("#layer_5", 1, {top: 400}, {top: 100, ease: Linear.easeNone}),
+      TweenMax.fromTo("#layer_6", 1, {top: 600}, {top: -100, ease: Linear.easeNone}),
+      TweenMax.fromTo("#layer_7", 1, {top: 350}, {top: 100, ease: Linear.easeNone})
+    ]);
+
+    // build scene
+    var scene = new ScrollMagic.Scene({triggerElement: ".piri", duration: document.body.clientHeight})
+    .setTween(tween)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+  },
+
+  registerNewsletter: function() {
+
+  },
+
+  registerSlider : function() {
+    var elem = document.getElementById('slider');
+    var flkty = new Flickity( elem, {
+      // options,
+      contain: true,
+      freeScroll: true,
+      autoPlay: true,
+      prevNextButtons: false,
+      pageDots: false
+    });
+  }
+};
+
+app.init();
